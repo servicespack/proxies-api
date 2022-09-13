640ce26ca3fd8d127fad19995977aa76b5677a9b
Gabriel Rufino <contato@gabrielrufino.com>
2022-09-13 08:14:19 -0300

    Complete CRUD

--- src/routers/proxies.router.js
+++ src/routers/proxies.router.js
@@ -25,9 +25,58 @@ router
     db.data.proxies.push(proxy)
     await db.write()
 
-    response.status(201).json(proxy)
-
     ProxiesEmitter.emitter.emit(ProxiesEmitter.Events.NEW_PROXY, proxy)
+    return response.status(201).json(proxy)
+  })
+  .get('/:proxyId', (request, response) => {
+    const { proxyId } = request.params
+    const proxy = db.data.proxies.find(({ id }) => id === proxyId)
+
+    if (!proxy) {
+      return response.status(404).json({ error: 'Not found' })
+    }
+
+    return response.json(proxy)
+  })
+  .patch('/:proxyId', async (request, response) => {
+    const { proxyId } = request.params
+    const proxyIndex = db.data.proxies.findIndex(({ id }) => id === proxyId)
+    const NOT_FOUND_INDEX = -1
+
+    if (proxyIndex === NOT_FOUND_INDEX) {
+      return response.status(404).json({ error: 'Not found' })
+    }
+
+    db.data.proxies = db.data.proxies.map(({ id, ...rest }) => {
+      if (id === proxyId) {
+        return {
+          ...rest,
+          ...request.body,
+          id
+        }
+      }
+
+      return { id, ...rest }
+    })
+    await db.write()
+
+    const proxy = db.data.proxies[proxyIndex]
+    ProxiesEmitter.emitter.emit(ProxiesEmitter.Events.UPDATED_PROXY, proxy)
+    return response.json(proxy)
+  })
+  .delete('/:proxyId', async (request, response) => {
+    const { proxyId } = request.params
+    const proxy = db.data.proxies.find(({ id }) => id === proxyId)
+
+    if (!proxy) {
+      return response.status(404).json({ error: 'Not found' })
+    }
+
+    db.data.proxies = db.data.proxies.filter(({ id }) => id !== proxyId)
+    await db.write()
+
+    ProxiesEmitter.emitter.emit(ProxiesEmitter.Events.DELETED_PROXY, proxy)
+    return response.json(proxy)
   })
 
 export const proxies = router

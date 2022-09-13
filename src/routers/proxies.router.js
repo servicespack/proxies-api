5c17e9463eac840eef9fcecf7582af94af5bfb18
Gabriel Rufino <contato@gabrielrufino.com>
2022-09-13 08:55:51 -0300

    Enable/Disable proxies crud

--- src/routers/proxies.router.js
+++ src/routers/proxies.router.js
@@ -7,76 +7,78 @@ import { db } from '../db.js'
 
 const router = Router()
 
-router
-  .get('/', (_request, response) => {
-    return response.json({
-      data: db.data.proxies
+if (process.env.ENABLE_PROXIES_CRUD === 'true') {
+  router
+    .get('/', (_request, response) => {
+      return response.json({
+        data: db.data.proxies
+      })
     })
-  })
-  .post('/', async (request, response) => {
-    const { namespace, target } = request.body
+    .post('/', async (request, response) => {
+      const { namespace, target } = request.body
 
-    const proxy = {
-      id: randomUUID(),
-      namespace,
-      target,
-      createdAt: new Date().toISOString()
-    }
-    db.data.proxies.push(proxy)
-    await db.write()
+      const proxy = {
+        id: randomUUID(),
+        namespace,
+        target,
+        createdAt: new Date().toISOString()
+      }
+      db.data.proxies.push(proxy)
+      await db.write()
 
-    ProxiesEmitter.emitter.emit(ProxiesEmitter.Events.NEW_PROXY, proxy)
-    return response.status(201).json(proxy)
-  })
-  .get('/:proxyId', (request, response) => {
-    const { proxyId } = request.params
-    const proxy = db.data.proxies.find(({ id }) => id === proxyId)
+      ProxiesEmitter.emitter.emit(ProxiesEmitter.Events.NEW_PROXY, proxy)
+      return response.status(201).json(proxy)
+    })
+    .get('/:proxyId', (request, response) => {
+      const { proxyId } = request.params
+      const proxy = db.data.proxies.find(({ id }) => id === proxyId)
 
-    if (!proxy) {
-      return response.status(404).json({ error: 'Not found' })
-    }
-
-    return response.json(proxy)
-  })
-  .patch('/:proxyId', async (request, response) => {
-    const { proxyId } = request.params
-    const proxyIndex = db.data.proxies.findIndex(({ id }) => id === proxyId)
-    const NOT_FOUND_INDEX = -1
-
-    if (proxyIndex === NOT_FOUND_INDEX) {
-      return response.status(404).json({ error: 'Not found' })
-    }
-
-    db.data.proxies = db.data.proxies.map(({ id, ...rest }) => {
-      if (id === proxyId) {
-        return {
-          ...rest,
-          ...request.body,
-          id
-        }
+      if (!proxy) {
+        return response.status(404).json({ error: 'Not found' })
       }
 
-      return { id, ...rest }
+      return response.json(proxy)
     })
-    await db.write()
+    .patch('/:proxyId', async (request, response) => {
+      const { proxyId } = request.params
+      const proxyIndex = db.data.proxies.findIndex(({ id }) => id === proxyId)
+      const NOT_FOUND_INDEX = -1
 
-    const proxy = db.data.proxies[proxyIndex]
-    ProxiesEmitter.emitter.emit(ProxiesEmitter.Events.UPDATED_PROXY, proxy)
-    return response.json(proxy)
-  })
-  .delete('/:proxyId', async (request, response) => {
-    const { proxyId } = request.params
-    const proxy = db.data.proxies.find(({ id }) => id === proxyId)
+      if (proxyIndex === NOT_FOUND_INDEX) {
+        return response.status(404).json({ error: 'Not found' })
+      }
 
-    if (!proxy) {
-      return response.status(404).json({ error: 'Not found' })
-    }
+      db.data.proxies = db.data.proxies.map(({ id, ...rest }) => {
+        if (id === proxyId) {
+          return {
+            ...rest,
+            ...request.body,
+            id
+          }
+        }
 
-    db.data.proxies = db.data.proxies.filter(({ id }) => id !== proxyId)
-    await db.write()
+        return { id, ...rest }
+      })
+      await db.write()
 
-    ProxiesEmitter.emitter.emit(ProxiesEmitter.Events.DELETED_PROXY, proxy)
-    return response.json(proxy)
-  })
+      const proxy = db.data.proxies[proxyIndex]
+      ProxiesEmitter.emitter.emit(ProxiesEmitter.Events.UPDATED_PROXY, proxy)
+      return response.json(proxy)
+    })
+    .delete('/:proxyId', async (request, response) => {
+      const { proxyId } = request.params
+      const proxy = db.data.proxies.find(({ id }) => id === proxyId)
+
+      if (!proxy) {
+        return response.status(404).json({ error: 'Not found' })
+      }
+
+      db.data.proxies = db.data.proxies.filter(({ id }) => id !== proxyId)
+      await db.write()
+
+      ProxiesEmitter.emitter.emit(ProxiesEmitter.Events.DELETED_PROXY, proxy)
+      return response.json(proxy)
+    })
+}
 
 export const proxies = router

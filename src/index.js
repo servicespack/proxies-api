dcb12b9daea48d62e19ec34e3ffcca6d9e314477
Gabriel Rufino <contato@gabrielrufino.com>
2024-07-03 19:17:55 -0300

    refactor: update lowdb

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import { cooldown } from './cooldown.js';
-import { db } from './db.js';
 import { logger } from './logger.js';
 import { server } from './server.js';
 
 async function main() {
   const { PORT = 3000 } = process.env;
-
-  await db.read();
-  db.data ||= { proxies: [] };
-  await db.write();
 
   server.listen(PORT, () => logger.info(`Listening on ${PORT}`));
 
